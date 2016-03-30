2c10e6aff4ee0064ea83b206c53ee7836d689384
Artem Kostiuk <postatum@gmail.com>
2016-03-30 11:28:44 +0300

    Include global conf in settings in scaffold

--- ramses/scaffolds/ramses_starter/+package+/__init__.py
+++ ramses/scaffolds/ramses_starter/+package+/__init__.py
@@ -2,6 +2,9 @@ from pyramid.config import Configurator
 
 
 def main(global_config, **settings):
+    # Update settings with global_config to allow overriding
+    # config file params with console params
+    settings.update(global_config)
     config = Configurator(settings=settings)
     config.include('ramses')
     return config.make_wsgi_app()
